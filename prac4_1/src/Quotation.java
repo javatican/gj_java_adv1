4bf2cec7409cfad028658a6025b7177cc4723f94
Ryan Nieh <pon.nieh@gmail.com>
2017-04-12 23:15:23 +0800

    prac 3 and 4

---
+++ prac4_1/src/Quotation.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ryannieh
+ */
+public class Quotation {
+    public String quote="Welcome to Oracle, the new home of Java!";
+    
+    public void display(){
+        System.out.println(quote);
+    }
+}
