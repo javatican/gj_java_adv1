30123c27d2069506493524e60e670a47345b155d
Ryan Nieh <javatican@users.noreply.github.com>
2017-05-20 23:58:02 +0800

    new projects

---
+++ clothing_proj/src/ClothingTest.java
@@ -0,0 +1,9 @@
+public class ClothingTest {
+   
+  public static void main (String args[]) {
+ 
+  Shirt myShirt = new Shirt(111, "Casual shirt", 'R', 49.99, 'M');
+  
+  myShirt.display();
+  } 
+}
